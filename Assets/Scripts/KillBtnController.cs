93ec5ac7eb097e4d71473d2e537c6e480942329d
eluda315 <michelle3hg@gmail.com>
2026-02-05 01:00:27 +0900

    feat: 스킬 쿨타임 UI 구현

--- Assets/Scripts/KillBtnController.cs
+++ Assets/Scripts/KillBtnController.cs
@@ -2,22 +2,54 @@ using Photon.Pun;
 using Photon.Realtime;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 public class KillBtnController : MonoBehaviourPunCallbacks
 {
+    [Header("UI 컴포넌트 연결")]
     [SerializeField] private Button killButton; // 킬 버튼
+    [SerializeField] private Image hideImage; // 비활성화 역할 스킬 이미지
+
+    [Header("스킬 설정")]
+    [SerializeField] private float coolTime = 10f; // 쿨타임 40초(임시)
+
+    private bool isCoolDown = false; // 스킬쿨 세는 중인지
     
+
     void Start()
     {
-        Debug.Log("KillBtnController.cs 시작");
-        
-        // 일단 시작하자마자 킬 버튼 비활성화
+        // 일단 시작하자마자 킬 버튼 비활성화(직업 확인 전이므로)
+        hideImage.gameObject.SetActive(false);
         killButton.gameObject.SetActive(false);
+
+        // 킬 버튼은 클릭 이벤트 연결
         killButton.onClick.AddListener(OnClickKillButton);
         
         // 직업 확인 후 살인자만 KILL button UI 활성화
         CheckJobAndActivateUI();
+    }
+
+    void Update()
+    {
+        // 스페이스바 누르는 순간을 감지
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("스페이스바 눌림!!");
+            OnClickKillButton();
+        }
+    }
+
+    #region [직업 체크 로직]
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        // 내가 + 직업이 배정(변경)되었다면
+        if(targetPlayer.IsLocal && changedProps.ContainsKey("Job"))
+        {
+            CheckJobAndActivateUI();
+        }
+        
     }
 
     public void CheckJobAndActivateUI()
@@ -31,25 +63,58 @@ public class KillBtnController : MonoBehaviourPunCallbacks
                 Debug.Log("킬러의 킬 버튼 활성화!");
                 killButton.gameObject.SetActive(true);
                 killButton.interactable = true;
+                
+                // 만약 게임 시작하자마자 스킬 사용 가능 시 hide image는 false
+                // 게임 시작 후 쿨타임 똑같이 지나야 스킬 버튼 활성화된다면 true로 변경하기
+                hideImage.gameObject.SetActive(false);
+
             }
         }
     }
 
-    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
-    {
-        // 내가 + 직업이 배정(변경)되었다면
-        if(targetPlayer.IsLocal && changedProps.ContainsKey("Job"))
-        {
-            CheckJobAndActivateUI();
-        }
-        
-    }
+    #endregion
 
+    #region [킬 버튼]
     public void OnClickKillButton()
     {
-        // 킬 버튼 이미지를 kill(active) -> kill(inactive)으로 변경
         // 킬 기능: 사거리 내 가장 가까운 플레이어 죽이기
-
         Debug.Log("킬 버튼 눌림!!");
+
+        if(isCoolDown) return; // 쿨타임 중이라면 무시
+
+        // !! 킬 로직 넣기 !!
+
+        Debug.Log("스킬 사용함. 쿨타임 시작");
+
+        // 일단 킬 성공했다는 가정 하에(킬 실패는 나중에 고려)
+        StartCoroutine(StartCoolDown()); 
+    }
+
+    IEnumerator StartCoolDown()
+    {
+        isCoolDown = true;
+        killButton.interactable = false;
+
+        // 덮는 이미지 켜서 360도 돌리기
+        hideImage.gameObject.SetActive(true);
+        hideImage.fillAmount = 1.0f; // 처음은 360도 꽉 채워서 시작
+
+        float cur = coolTime; // 쿨타임 40초로 시작해서
+        while (cur > 0) 
+        {
+            cur -= Time.deltaTime; // 시간 계속 깍기
+            hideImage.fillAmount = cur / coolTime; // 360도에서 점점 돌기
+
+            yield return null; // 1프레임 대기
+        }
+
+        // 쿨타임 종료 후 다시 버튼 활성화
+        isCoolDown = false;
+        killButton.interactable = true;
+        hideImage.gameObject.SetActive(false);
+
+        Debug.Log("쿨타임 종료.");
     }
 }
+
+#endregion
